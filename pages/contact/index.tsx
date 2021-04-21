1f88828c9e962813f69bd23e2dd723c0534965b2
Denis Zuykov <dendjet@gmail.com>
2021-04-21 23:58:49 +0300

    feat: contacts page

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -11,7 +11,6 @@ import Typography, {
 import Section from '../../components/Section/Section'
 import StandardImage from '../../components/StandardImage/StandardImage'
 import Button, { ButtonTheme } from '../../components/Button/Button'
-import Leader from '../../components/Leader/Leader'
 import GithubIcon from '../../components/GithubIcon/GithubIcon'
 import { AlphabetSize } from '../../@types/common'
 import { useDispatch } from 'react-redux'
@@ -40,7 +39,7 @@ const ContactPage: NextPage<PageProps> = ({}: PageProps) => {
         />
       </Head>
 
-      <Section dark className={style.intro}>
+      <Section className={style.intro}>
         <Container>
           <Grid container justify="center">
             <Grid item xs={12} md={12}>
@@ -102,145 +101,6 @@ const ContactPage: NextPage<PageProps> = ({}: PageProps) => {
                   Московская обл.,
                   <br /> г. Люберцы, ул. Кирова, д. 20А
                 </RLink>
-              </div>
-            </Grid>
-          </Grid>
-        </Container>
-      </Section>
-      <Section dark>
-        <Container>
-          <Grid container justify="center">
-            <Grid item xs={12} md={8}>
-              <Heading weight={2} noMt>
-                Наша технология
-              </Heading>
-              <Typography size={24} mb={48}>
-                В&nbsp;мире еще нет такой системы, который&nbsp;бы управлял
-                только искусственный интеллект. ИИ&nbsp;все еще несовершенен,
-                он&nbsp;ошибается и&nbsp;не&nbsp;способен закрыть сложные
-                задачи, с&nbsp;которыми успешно справляется человек.
-                Мы&nbsp;в&nbsp;Dbrain верим, что у&nbsp;ИИ есть потенциал
-                и&nbsp;будущее, но&nbsp;пока результаты его работы требуют
-                проверки.
-              </Typography>
-              <Typography theme={TypographyTheme.Blue} mb={48}>
-                Система Dbrain самосовершенствуется
-              </Typography>
-              <Typography size={24} mb={72}>
-                Мы&nbsp;используем технологию human-in-the-loop для исправления
-                ошибок ИИ. Когда система совершает оплошность, квалифицированные
-                разметчики данных исправляет ее&nbsp;и&nbsp;отправляет результат
-                обратно в&nbsp;систему, чтобы она не&nbsp;ошиблась
-                в&nbsp;будущем. А&nbsp;сложные случаи&nbsp;&mdash; например,
-                рукописный текст&nbsp;&mdash; сразу отдаются людям. Так нам
-                удается закрыть даже самые сложные случаи с&nbsp;высоким
-                качеством.
-              </Typography>
-              <Grid container>
-                <Grid xs={12} md={6}>
-                  <a
-                    href="https://github.com/dbrainio"
-                    target="_blank"
-                    className={style.github}
-                  >
-                    <Button
-                      theme={ButtonTheme.Blue}
-                      icon={<GithubIcon />}
-                      size={AlphabetSize.L}
-                    >
-                      Dbrain на GitHub
-                    </Button>
-                  </a>
-                </Grid>
-                <Grid xs={12} md={6}>
-                  <a
-                    href="https://toloka.yandex.ru/"
-                    target="_blank"
-                    className={style.toloka}
-                  >
-                    <StandardImage
-                      src="/images/toloka.png"
-                      className={style.tolokaImage}
-                    />
-                    <Typography> Партнер Яндекс.Толоки</Typography>
-                  </a>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Container>
-      </Section>
-      <Section className={style.leadersSection}>
-        <Container>
-          <Grid container justify="center">
-            <Grid item xs={12} md={8}>
-              <Heading weight={2} noMt>
-                Лидеры Dbrain
-              </Heading>
-              <div className={style.leaders}>
-                <Grid container>
-                  {[
-                    {
-                      src: '/images/dm.jpg',
-                      name: 'Дмитрий Мацкевич',
-                      title: 'Основатель, CEO',
-                      linkedIn: 'https://www.linkedin.com/in/mdima/',
-                    },
-                    {
-                      src: '/images/ah.jpg',
-                      name: 'Алексей Хахунов',
-                      title: 'Основатель, CTO',
-                      linkedIn: 'https://www.linkedin.com/in/alexeihahunov/',
-                    },
-                    {
-                      src: '/images/at.jpg',
-                      name: 'Алексей Цишевский',
-                      title: 'COO',
-                      linkedIn: 'https://www.linkedin.com/in/tsish/',
-                    },
-                  ].map((l) => (
-                    <Grid item xs={12} md={4}>
-                      <div className={style.leader}>
-                        <Leader {...l} />
-                      </div>
-                    </Grid>
-                  ))}
-                </Grid>
-              </div>
-              <Button
-                size={AlphabetSize.L}
-                theme={ButtonTheme.Blue}
-                onClick={openContactUs}
-              >
-                Связаться
-              </Button>
-            </Grid>
-          </Grid>
-        </Container>
-      </Section>
-      <Section dark>
-        <Container>
-          <Grid container justify="center">
-            <Grid item xs={12} md={8}>
-              <Heading weight={2} noMt>
-                Присоединяйся к&nbsp;команде
-              </Heading>
-              <Typography size={24} mb={48}>
-                В&nbsp;нашей команде собрались умные и&nbsp;талантливые люди
-                (ex-Yandex, Uber и&nbsp;др.), которые объединены общей
-                целью&nbsp;&mdash; создавать технологии будущего. Мы&nbsp;всегда
-                открыты для новых людей&nbsp;&mdash; присоединяйся!
-              </Typography>
-              <Typography theme={TypographyTheme.Blue} mb={72}>
-                Love people. Use things. <br />
-                The opposite never works
-              </Typography>
-              <div className={style.jobs}>
-                <a href="https://jobs.dbrain.io/" target="_blank">
-                  <Button theme={ButtonTheme.Blue} size={AlphabetSize.L}>
-                    Вакансии
-                  </Button>
-                </a>
               </div>
             </Grid>
           </Grid>
